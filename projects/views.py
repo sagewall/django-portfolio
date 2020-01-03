a0408162d51bab6b24a836f6541adcc67fa29ac7
Sage Wall <sagewall@users.noreply.github.com>
2020-01-03 11:11:00 -0700

    Create a template

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+
 def project_list(request):
-    return HttpResponse('<h1>Hello world!</h1>')
+    return render(request, 'projects/index.html')
