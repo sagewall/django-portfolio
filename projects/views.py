1ef92a281969d0c04454815bad975000b11fdf0f
Sage Wall <sagewall@users.noreply.github.com>
2020-01-08 11:52:46 -0700

    Add detail route

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,8 @@ from projects.models import Project
 def all_projects(request):
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
